aaa6df83beebe90edbf6c724af0c196e5fed0797
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-15 10:13:24 +0100

    finished creating files & templates

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,29 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { eventBus } from './main.js'
+import BeerDetail from './components/BeerDetail.vue'
+import BeerList from './components/BeerList.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      beers: [],
+      seelectedBeer: null,
+    };
+  },
+  mounted() {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(res = res.json())
+    .then(beers => this.beers = data)
+
+    eventBus.$on('beer-selected', (beer) => {
+      this.selectedBeer = beer
+    })
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
